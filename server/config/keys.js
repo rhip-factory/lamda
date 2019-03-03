38cb62b5223f9baf74898f65473b3625d1a43945
abdulbasitsaid <mrspeed619@gmai.com>
2019-03-03 10:40:42 +0100

    majorUpdate

--- server/config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI: "mongodb://abdulbasit:rambooze007@ds357955.mlab.com:57955/lamda",
-  secretOrKey: "lamdasecret"
-};
--- server/routes/api/transactions.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-//@route GET api/users/
-//@desc Default
-//@access Public
-router.get("/", (req, res) => {
-    res.json("Default");
-  });
-  module.exports = router;
